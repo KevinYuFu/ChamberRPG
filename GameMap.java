75c0265622cc09241c599cac7ba49a0793d1b743
KaiZen <kevin.yu.fu@gmail.com>
2016-08-13 23:57:39 -0400

    Add Begining files to work with. GameMap object will contain data on the playing field for the players and such as it's bounds A point object is just a x and y position

---
+++ GameMap.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+
+public class GameMap{
+
+	public GameMap(){
+
+	}
+
+	// Checks co-ordinates for given points.
+	// returns true if points are valid
+	public boolean inBound(){
+	}
+}
